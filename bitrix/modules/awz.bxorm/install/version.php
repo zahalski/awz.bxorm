c33c443b1d0f2ea69215a46130e82c0e0e29358c
Andrew Zahalski <info@zahalski.dev>
2023-07-24 13:04:21 +0300

    - изменено название модуля

--- bitrix/modules/awz.bxorm/install/version.php
+++ bitrix/modules/awz.bxorm/install/version.php
@@ -1,6 +1,6 @@
 <?
 $arModuleVersion = array(
-	"VERSION" => "1.0.4",
-	"VERSION_DATE" => "2023-07-23 18:30:00"
+	"VERSION" => "1.0.5",
+	"VERSION_DATE" => "2023-07-24 13:03:00"
 );
 ?>
--- bitrix/modules/awz.bxorm/lang/ru/options.php
+++ bitrix/modules/awz.bxorm/lang/ru/options.php
@@ -1,5 +1,5 @@
 <?
-$MESS["AWZ_BXORM_OPT_TITLE"] = "Настройки модуля AWZ: ORM Api";
+$MESS["AWZ_BXORM_OPT_TITLE"] = "Настройки модуля AWZ: Api на вебхуках";
 $MESS["AWZ_BXORM_OPT_SECT1"] = "Общие настройки";
 $MESS["AWZ_BXORM_OPT_SECT3"] = "Права доступа";
 $MESS["AWZ_BXORM_OPT_L_BTN_SAVE"] = "Сохранить";
